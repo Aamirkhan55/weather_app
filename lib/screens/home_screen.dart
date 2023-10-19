27e09526bd98d83ea3dd378d7b185c7faca772c2
Amir55143 <aamirnoor55143@gmail.com>
2023-10-19 10:54:16 +0500

    BackDropFilter

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -38,6 +40,51 @@ class _HomeScreenState extends State<HomeScreen> {
                   ),
                 ),
               ),
+               Align(
+                alignment: const AlignmentDirectional(-3, -0.3),
+                child: Container(
+                  height: 300,
+                  width: 300,
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Color(0xFF673AB7),
+                  ),
+                ),
+              ),
+               Align(
+                alignment: const AlignmentDirectional(0, -1.2),
+                child: Container(
+                  height: 300,
+                  width: 600,
+                  decoration: const BoxDecoration(
+                    color: Color(0xFFFFAB40),
+                  ),
+                ),
+              ),
+              BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 100.0, sigmaY: 100.0),
+                child: Container(
+                  decoration: const BoxDecoration(
+                    color: Colors.transparent,
+                  ),
+                ),
+                ),
+              SizedBox(
+                width: MediaQuery.sizeOf(context).width,
+                height: MediaQuery.sizeOf(context).height,
+                child: const Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Aix-en-Provence',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w300
+                      ),
+                      )
+                  ],
+                ),
+              ),  
             ],
           ),
         ),
