a2b010985089f4f0d9a3ccd484bb8cb6c1396a39
Amir55143 <aamirnoor55143@gmail.com>
2023-10-19 15:45:37 +0500

    WeatherIcon

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -14,6 +14,42 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  Widget getWeatherIcon(int code) {
+		switch (code) {
+		  case >= 200 && < 300 :
+		    return Image.asset(
+					'assets/1.png'
+				);
+			case >= 300 && < 400 :
+		    return Image.asset(
+					'assets/2.png'
+				);
+			case >= 500 && < 600 :
+		    return Image.asset(
+					'assets/3.png'
+				);
+			case >= 600 && < 700 :
+		    return Image.asset(
+					'assets/4.png'
+				);
+			case >= 700 && < 800 :
+		    return Image.asset(
+					'assets/5.png'
+				);
+			case == 800 :
+		    return Image.asset(
+					'assets/6.png'
+				);
+			case > 800 && <= 804 :
+		    return Image.asset(
+					'assets/7.png'
+				);
+		  default:
+			return Image.asset(
+				'assets/7.png'
+			);
+		}
+	}
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -94,7 +130,7 @@ class _HomeScreenState extends State<HomeScreen> {
                                 color: Colors.white,
                                 fontWeight: FontWeight.bold),
                           ),
-                          Image.asset('assets/1.png'),
+                          getWeatherIcon(state.weather.weatherConditionCode!),
                           const SizedBox(height: 8),
                            Center(
                             child: Text(
